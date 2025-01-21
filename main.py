b8e01ee7e144d9386837349e46f223c36c27d09f
Alhassan Ibrahim <zerosoftwere@gmail.com>
2025-01-21 23:58:42 +0100

    Feature: Dup test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify
-from os import environ
 
 app = Flask(__name__)
 
@@ -12,14 +11,6 @@ def index():
 @app.route('/health')
 def health():
     return jsonify({'status': 'UP'})
-
-def calculate():
-    print('Calculating...')
-    return 1 + 1
-
-def calculate2():
-    print('Calculating...')
-    return 1 + 1
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,22 @@
 from main import app
 import pytest
 
+
 @pytest.fixture
 def client():
     """A test client for the app."""
     with app.test_client() as client:
         yield client
 
+
 def test_index(client):
     """Test the index route."""
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
+
 
 def test_health(client):
     """Test the index route."""
-    response = client.get('/health')
+    response = client.get("/health")
     assert response.status_code == 200
-    assert response.json == {'status': 'UP'}
+    assert response.json == {"status": "UP"}
