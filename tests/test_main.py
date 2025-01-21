db1ad06a2b000e0c9331d6ee61506f2157a1c276
Alhassan Ibrahim <zerosoftwere@gmail.com>
2025-01-22 00:10:32 +0100

    Feature: Dup test

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,12 +11,12 @@ def client():
 
 def test_index(client):
     """Test the index route."""
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
 
 
 def test_health(client):
     """Test the index route."""
-    response = client.get("/health")
+    response = client.get('/health')
     assert response.status_code == 200
-    assert response.json == {"status": "UP"}
+    assert response.json == {'status': 'UP'}
